1d10630498a1d9d06f25c5820b5ffad32617d910
serrve1 <gustjqdl5056@gmail.com>
2024-01-31 14:11:13 +0900

    hello.html

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ templates = Jinja2Templates(directory="templates")
 async def main(request: Request):
     return templates.TemplateResponse(request=request,name="index.html", context ={"name": "김태뿅"})
 
-@app.get('/hello')
-async def hello():
-    return "안녕! 반가워!"
+# 경로를 바꿉니다.
+# 다른 html을 넣어줘야함
+#{name}을 경로에 넣어주면 ==> async def name으로 간다.
+@app.get('/hello/{name}', response_class=HTMLResponse)
+
+# context 딕셔너리의 키값 "name"이랑 hello.html에 있는 name과 연결되어있는것이다.
+async def hello(request: Request, name):
+    words = f'당신의 이름은 {name}니까 반가워서 인사'
+    return templates.TemplateResponse(request=request,name="hello.html", context ={"name": words})
