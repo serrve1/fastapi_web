00314c15da3f0c9e40e9b7c358cda34af187252f
serrve1 <gustjqdl5056@gmail.com>
2024-01-31 13:17:53 +0900

    index.html

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World!!"}
+@app.get('/', response_class=HTMLResponse)
+async def main(request: Request):
+    return templates.TemplateResponse(request=request,name="index.html")
 
+
+@app.get('/hello')
+async def hello():
+    return "안녕! 반가워!"
